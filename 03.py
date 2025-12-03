068188595ca494c78a9b0b1da2c53817fb3c0a71
emiruz <hi@emiruz.com>
2025-12-03 22:03:56 +0100

    Day 1-3

--- 03.py
+++ 03.py
@@ -1,9 +1,9 @@
 xs  = [x for x in open("input.txt","r").read().split("\n") if x]
 
-def f(xs, i=0, n=-11):
+def f(xs, i, n):
     j = i + xs[i:].index(max(xs[i:(None if n == 0 else n)]))
     if n == 0: return [xs[j]]
     return [xs[j]] + f(xs, j + 1, n + 1)
 
 print("Part #1:", sum([int("".join(f(x, 0, -1))) for x in xs]))
-print("Part #2:", sum([int("".join(f(x))) for x in xs]))
+print("Part #2:", sum([int("".join(f(x, 0, -11))) for x in xs]))
